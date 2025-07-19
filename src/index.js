b9f19facdedb292b551e8a0316db9055a9429fc0
Faraz <faraz.com>
2025-07-19 18:28:17 +0530

    Added eslint to fix lint problem

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import express from "express";
-import { StatusCodes } from "http-status-codes";
+import express from 'express';
+import { StatusCodes } from 'http-status-codes';
 
-import { PORT } from "./config/serverConfig.js";
+import { PORT } from './config/serverConfig.js';
+
 const app = express();
-app.get("/ping", (req, res) => {
-  return res.status(StatusCodes.OK).json({ message: "pongs" });
+app.get('/ping', (req, res) => {
+  return res.status(StatusCodes.OK).json({ message: 'pongs' });
 });
 app.listen(PORT, () => {
-  console.log("server is runnin on " + PORT);
+  console.log('server is runnin on ' + PORT);
 });
