3bf07d6a71dc1ebb7d97ecdfde32e2f999fb380f
Faraz <faraz.com>
2025-07-19 18:28:17 +0530

    Added eslint to fix lint problem

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,25 @@
 import js from "@eslint/js";
-import globals from "globals";
 import { defineConfig } from "eslint/config";
-
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.node },
+  },
 ]);
