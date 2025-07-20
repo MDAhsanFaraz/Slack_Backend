acff01ed5b19c0a0b9b6c5c9f02ed4c694c2f46a
Faraz <faraz.com>
2025-07-20 05:52:40 +0530

    Added signup

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,24 +1,27 @@
 import { StatusCodes } from 'http-status-codes';
-import { signUpService } from '../service/userService';
-import {
-  customErrorresponse,
-  internalErrorresponse,
-  successResponse
-} from '../utils/errors/common/responseObjects';
 
-export const signup = async function (req, res) {
+import { signUpService } from '../services/userService.js';
+import {
+  customErrorResponse,
+  internalErrorResponse,
+  successResponse
+} from '../utils/common/responseObjects.js';
+
+export const signUp = async (req, res) => {
   try {
     const user = await signUpService(req.body);
+
     return res
       .status(StatusCodes.CREATED)
       .json(successResponse(user, 'User created successfully'));
   } catch (error) {
     console.log('User controller error', error);
     if (error.statusCode) {
-      return res.status(error.statusCode).json(customErrorresponse(error));
+      return res.status(error.statusCode).json(customErrorResponse(error));
     }
+
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json(internalErrorresponse(error));
+      .json(internalErrorResponse(error));
   }
 };
