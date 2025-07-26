b7a747d41b8da1a6f812d525e1d631c0cecd2f4d
Faraz <faraz.com>
2025-07-26 20:30:01 +0530

    jwt auth token verification added

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,46 @@
+import { StatusCodes } from 'http-status-codes';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../config.js';
+import {
+  customErrorResponse,
+  internalErrorResponse
+} from '../utils/common/responseObjects.js';
+import userRepository from '../repositories/userRepository.js';
+export const isAuthenticated = async (req, res, next) => {
+  try {
+    const token = req.headers['x-access-token'];
+    if (!token) {
+      return res.status(StatusCodes.FORBIDDEN).json(
+        customErrorResponse({
+          explanaiton: 'Invalid data sent from the client',
+          message: 'Invalid auth token provided'
+        })
+      );
+    }
+    const response = jwt.verify(token, JWT_SECRET);
+    if (!response) {
+      return res.status(StatusCodes.FORBIDDEN).json(
+        customErrorResponse({
+          explanaiton: 'Invalid data sent from the client',
+          message: 'Invalid auth token provided'
+        })
+      );
+    }
+    const user = await userRepository.getById(response.id);
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log('Auth middleware error', error);
+    if (error.name === 'JsonWebTokenError') {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(
+        customErrorResponse({
+          explanation: 'An error occurred while processing the request',
+          message: 'Invalid auth token provided '
+        })
+      );
+    }
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(internalErrorResponse(error));
+  }
+};
