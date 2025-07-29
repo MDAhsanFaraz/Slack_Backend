e06b61bdcd401da538604b7c858a1e161e9acb4e
Faraz <faraz.com>
2025-07-30 00:21:37 +0530

    Added addMemberToWorkspace fucntion

--- src/repositories/workspaceRepository.js
+++ src/repositories/workspaceRepository.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import Workspace from '../schema/workspace';
 import ClientError from '../utils/errors/clientError';
 import crudRepository from './crudRepository';
+import User from '../schema/user';
 
 const workspaceRepository = {
   ...crudRepository(Workspace),
@@ -29,7 +30,36 @@ const workspaceRepository = {
     }
     return workspace;
   },
-  addMemberToWorkspace: async function () {},
+  addMemberToWorkspace: async function (memberId, role, workspaceId) {
+    const workspace = Workspace.findById({ workspaceId });
+    if (!workspace) {
+      throw new ClientError({
+        explanation: 'Invalid data sent from the client',
+        message: 'Workspace not found',
+        statusCode: StatusCodes.NOT_FOUND
+      });
+    }
+    const isValidUser = await User.findById({ memberId });
+    if (!isValidUser) {
+      throw new ClientError({
+        explanation: 'Invalid data sent from the client',
+        message: 'User not found',
+        statusCode: StatusCodes.NOT_FOUND
+      });
+    }
+    const isMemberAlreadyPartOfWorkspace = workspace.members.find(
+      (member) => member.memberId == memberId
+    );
+    if (!isMemberAlreadyPartOfWorkspace) {
+      throw new ClientError({
+        explanation: 'Invalid data sent from the client',
+        message: 'User already part of workspace',
+        statusCode: StatusCodes.FORBIDDEN
+      });
+    }
+    workspace.members.push({ memberId, role });
+    return workspace;
+  },
   addChannelToWorkspace: async function () {},
   fechAllWorkspaceByMemberId: async function () {}
 };
