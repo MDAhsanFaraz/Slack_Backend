c580fde5983d1b7e29034239cacf58fe5e3a8e5f
Faraz <faraz.com>
2025-07-30 00:50:39 +0530

    Added workspace repo

---
+++ src/repositories/channelRepository.js
@@ -0,0 +1,5 @@
+import Channel from '../schema/channel';
+import crudRepository from './crudRepository';
+
+const channelRepository = { ...crudRepository(Channel) };
+export default channelRepository;
