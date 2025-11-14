ebfd26c81c748717ba8cf6178d4eaacc3f23e0ba
Faraz <faraz.com>
2025-11-15 03:24:40 +0530

    Added member api to check subscription of a workspace

---
+++ src/routes/v1/members.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { isMemberPartOfWorkSpaceController } from '../../controllers/memberController.js';
+import { isAuthenticated } from '../../middleware/authMiddleware.js';
+
+const router = express.Router();
+router.get(
+  '/workspace/:workspaceId',
+  isAuthenticated,
+  isMemberPartOfWorkSpaceController
+);
+export default router;
