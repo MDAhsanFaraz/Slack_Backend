dd84fba0654b0b49a9b3334658a7ccfa463a6a0f
Faraz <faraz.com>
2025-11-17 18:14:23 +0530

    Added socket.io setup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 import express from 'express';
+import { createServer } from 'http';
 import { StatusCodes } from 'http-status-codes';
+import { Server } from 'socket.io';
 
 import connectDB from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
 import apiRouter from './routes/apiRoutes.js';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRouter);
 app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'pongs' });
 });
-app.listen(PORT, () => {
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+  socket.on('connection1', (data) => {
+    console.log('message from client', data);
+    io.emit('newmessage', data.toUpperCase());
+  });
+});
+server.listen(PORT, () => {
   console.log('server is runnin on ' + PORT);
   connectDB();
 });
